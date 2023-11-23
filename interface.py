d1c04f841e35c7b150a69d72889045586a9768bd
Noah Collinet <noah.collinet1@gmail.com>
2023-11-23 13:51:31 +0100

    Interface Results Display

--- interface.py
+++ interface.py
@@ -1,12 +1,12 @@
-import tkinter
+import tkinter as tk
 import customtkinter as ctk
-# from utils import *
-# from test import * 
+from utils import *
+from test_output import *
+from matching import *
 
-ctk.set_appearance_mode("dark")  # Modes: system (default), light, dark
-ctk.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
 
-#Create the CTk window 
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
@@ -16,35 +16,42 @@ class App(ctk.CTk):
         self.grid_columnconfigure((0, 1), weight=1)
 
         def click_handler():
-            print(f"Getting {Input.get()} schools and students")
+            input_value = self.Input.get()
 
-        #Create a CTk Button
-        self.button = ctk.CTkButton(master=self, text="Get matching", command=click_handler,
-                       fg_color="transparent", border_width=2, border_color='#15869d')
-        self.button.place(relx=0.25, rely=0.25, anchor=ctk.CENTER)
+            try:
+                input_value = int(input_value)  # Convert input to integer (assuming it's a number)
+                result_file = "matching_results.txt"
+                random_test(input_value, input_value, result_file)  # Use the input as both student_number and school_number
 
-        #Create CTk checkboxes
-        self.checkbox_1 = ctk.CTkCheckBox(self, text="checkbox 1")
-        self.checkbox_1.grid(row=1, column=0, padx=20, pady=(0, 20), sticky="w")
-        self.checkbox_2 = ctk.CTkCheckBox(self, text="checkbox 2")
-        self.checkbox_2.grid(row=1, column=1, padx=20, pady=(0, 20), sticky="w")
+                # Read the results from the file
+                with open(result_file, "r") as file:
+                    result_text = file.read()
 
-        #Create an input
+                # Update the output label
+                self.output_label.configure(text=result_text)
+
+            except ValueError:
+                print("Invalid input. Please enter a valid number.")
+
+        # Create an input
         self.Input = ctk.CTkEntry(self, width=250, height=30, placeholder_text="Enter a number of students or schools")
-        self.Input.grid(row=2, padx=5, pady=10, sticky ="ew")
+        self.Input.grid(row=0, padx=5, pady=10, sticky="ew")
 
-        #Display text
-        self.text1 = ctk.CTkLabel(self, text='Mariage stable entre étudiants et établissements')
-        self.text1.grid(row=3, padx=5, pady=10, sticky ="ew")
-    
-app = App()
-#Create frames in the app
-#frame1 = ctk.CTkFrame(master=app,
-#                         height= app.winfo_height()*0.33,
-#                         width = app.winfo_width()*0.33,
-#                         fg_color="white")
-#frame1.place(relx=0.33, rely=0.025)
+        # Create a CTk Button
+        self.button = ctk.CTkButton(
+            master=self, text="Get matching", command=click_handler,
+            fg_color="transparent", border_width=2, border_color='#15869d'
+        )
+        self.button.grid(row=1, padx=5, pady=10, sticky="ew")
 
+        # Create a label for displaying the result
+        self.output_label = ctk.CTkLabel(self, text='', justify='left', height=10)  # Set height to allow for multiple lines
+        self.output_label.grid(row=2, padx=5, pady=10, sticky="ew")
 
-#Run the app
-app.mainloop()
+        # Placeholder for students and schools (you need to define these variables)
+        self.students = np.array([])
+        self.schools = np.array([])
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
---
+++ test_output.py
@@ -0,0 +1,124 @@
+from matching import *
+from utils import *
+import sys
+
+def is_everyone_matched(matches: list[tuple[int, int]], students: np.ndarray, schools: np.ndarray) -> bool:
+    """
+    Tests if each student and each school has a match in matches
+
+    :param matches : (list[tuple[int,int]]) A list of (student,school) tuples representing matches between the two sets
+    :param students : a Numpy array of student_number arrays, each being a an ordered preference array of school IDs
+    :param schools : a Numpy array of school_number arrays, each being an ordered preference array of student IDs
+    :return: (bool) True if everyone has a match, False if there is at least one student/school without a match
+    """
+
+    all_matched = True
+    for st_index in range(len(students)):
+        if get_matched_school(st_index, matches) < 0:
+            print(f'Student {st_index} is unmatched')
+            all_matched = False
+    for sc_index in range(len(schools)):
+        if get_matched_student(sc_index, matches) < 0:
+            print(f'School {sc_index} is unmatched')
+            all_matched = False
+    return all_matched
+
+
+def is_matching_stable(matches: list[tuple[int, int]], students: np.ndarray, schools: np.ndarray) -> bool:
+    """
+    Tests if the given matching is stable with regard to the set of preferences.
+    A matching is said unstable if there exists a student A and a school B such that :
+    1) A prefers B over its current match
+    and
+    2) B also prefers A over its current match
+
+    :param matches : (list[tuple[int,int]]) A list of (student,school) tuples representing matches between the two sets
+    :param students : a Numpy array of student_number arrays, each being a an ordered preference array of school IDs
+    :param schools : a Numpy array of school_number arrays, each being an ordered preference array of student IDs
+    :return: (bool) True if the matching is stable, False if there is at least one unstable pairing
+    """
+
+    stable = True
+    for st_index in range(len(students)):
+        matched_school = get_matched_school(st_index, matches)
+        for preferred_school in students[st_index]:
+            if preferred_school == matched_school:
+                break
+            matched_student = get_matched_student(preferred_school, matches)
+            if prefers(st_index, matched_student, schools[preferred_school]):
+                print(
+                    f'Unstability : student {st_index} prefers school {preferred_school} over {matched_school} and school {preferred_school} prefers student {st_index} over {matched_student}')
+                stable = False
+    return stable
+
+
+def random_test(student_number, school_number, result_file="matching_results.txt"):
+    """
+    Runs the matching algorithm on a randomly generated set of preferences and test the validity of the matching
+
+    :param student_number: (int) Number of students
+    :param school_number: (int) Number of schools
+    :param result_file: (str) File to save the matching results
+    :return: (bool) True if the matching returned is valid (complete and stable), False otherwise
+    """
+    students, schools = random_preferences(student_number, school_number)
+    print("Students :")
+    print(students)
+    print("Schools :")
+    print(schools)
+
+    matches = stable_marriage(students, schools)
+
+    print("Matching found :")
+    print(matches)
+
+    if not is_everyone_matched(matches, students, schools):
+        print("Warning : Not everyone is matched")
+        complete = False
+    else:
+        complete = True
+
+    if not is_matching_stable(matches, students, schools):
+        print("Matching is unstable")
+        return False
+
+    student_scores, school_scores = satisfaction(matches, students, schools)
+    print(student_scores, school_scores)
+    student_avg = np.mean(student_scores)
+    school_avg = np.mean(school_scores)
+    print(f"Student average satisfaction : {student_avg:.3f}")
+    print(f"School average satisfaction : {school_avg:.3f}")
+
+    if complete:
+        print("Matching is complete and stable")
+    else:
+        print("Matching is stable, but not complete")
+
+    # Save results to a file
+    with open(result_file, "w") as file:
+        file.write(f"Matching results:\n")
+        for student, school in matches:
+            file.write(f"Student {student} matches with School {school}\n")
+
+        file.write(f"\nStudent average satisfaction: {student_avg:.3f}\n")
+        file.write(f"School average satisfaction: {school_avg:.3f}\n")
+
+    return True
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print(f'Use : python -m test.test student_number school_number batch_size')
+
+    student_number = int(sys.argv[1])
+    school_number = int(sys.argv[2])
+    test_batch_size = int(sys.argv[3])
+
+    successes = 0
+
+    for i in range(test_batch_size):
+        print(f'===================\nTest {i}')
+        if random_test(student_number, school_number):
+            successes += 1
+
+    print(f'===================\n{successes}/{test_batch_size} passed')
